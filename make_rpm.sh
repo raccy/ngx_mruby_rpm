c9ed335cecacd27ffc8b2331a3626f377d189106
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-09-30 14:00:41 +0900

    ndkを組み込みのものに変えた

--- make_rpm.sh
+++ make_rpm.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+NGX_MRUBY_VERSION=2.2.5
+
 mkdir work
 cd work
 dnf download --source nginx
 rpm -ivh --define="%_topdir `pwd`/rpmbuild" nginx-*.src.rpm
-wget -O rpmbuild/SOURCES/ngx_mruby-2.2.5.tar.gz https://github.com/matsumotory/ngx_mruby/archive/refs/tags/v2.2.5.tar.gz
-wget -O rpmbuild/SOURCES/ngx_devel_kit-0.3.1.tar.gz https://github.com/vision5/ngx_devel_kit/archive/refs/tags/v0.3.1.tar.gz
+curl -L -o rpmbuild/SOURCES/ngx_mruby-${NGX_MRUBY_VERSION}.tar.gz https://github.com/matsumotory/ngx_mruby/archive/refs/tags/v${NGX_MRUBY_VERSION}.tar.gz
 patch -p1 -d rpmbuild/SPECS < ../nginx.spec.patch
 rpmbuild  --define="%_topdir `pwd`/rpmbuild" -ba rpmbuild/SPECS/nginx.spec
 cp rpmbuild/RPMS/x86_64/nginx-mod-http-mruby-1.*.rpm .
